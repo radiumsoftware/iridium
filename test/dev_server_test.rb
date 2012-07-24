3f7f92a875b9c192784cbd29ce6ba37b2ab55eca
twinturbo <me@broadcastingadam.com>
2012-07-24 15:59:25 +0200

    Add tests for dev server pipeline

--- test/dev_server_test.rb
+++ test/dev_server_test.rb
@@ -8,7 +8,18 @@ class DevServerTest < MiniTest::Unit::TestCase
   end
 
   def setup
+    @created_files = []
     Iridium.application = TestApp.instance
+  end
+
+  def teardown
+    FileUtils.rm_rf TestApp.root.join("app")
+    FileUtils.rm_rf TestApp.root.join("site")
+    FileUtils.rm_rf TestApp.root.join("tmp")
+
+    @created_files.each do |path|
+      FileUtils.rm_rf path
+    end
   end
 
   def test_it_should_serve_the_root
@@ -21,5 +32,28 @@ class DevServerTest < MiniTest::Unit::TestCase
     get '/foo/bars.js'
 
     assert_equal 404, last_response.status
+  end
+
+  def test_pipeline
+    create_file "javascripts/foo.js", "bar"
+
+    get '/application.js'
+
+    assert last_response.ok?
+
+    assert_includes last_response.body, "bar"
+  end
+
+  private
+  def create_file(path, content)
+    full_path = TestApp.root.join "app", path
+
+    @created_files << full_path
+
+    FileUtils.mkdir_p File.dirname(full_path)
+
+    File.open full_path, "w" do |f|
+      f.puts content
+    end
   end
 end
