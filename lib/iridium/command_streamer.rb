5a636725b4d395b2946067ba1eac485f2ca33cd7
twinturbo <me@broadcastingadam.com>
2012-08-08 11:18:44 +0200

    Attempt to fix debian

--- lib/iridium/command_streamer.rb
+++ lib/iridium/command_streamer.rb
@@ -1,17 +1,34 @@
 require 'pty'
 require 'json'
 
+
 module Iridium
   class CommandStreamer
     class CommandFailed < RuntimeError ; end
     class ProcessAborted < RuntimeError ; end
+
+    # PTY IO is platform dependent on. Reading raises a Errno:EIO 
+    # as regular behavior on some platforms. Wrap it up here so
+    # things behave like developers expect
+    class SafePTY
+      def self.spawn(command, &block)
+        PTY.spawn(command) do |r,w,p|
+          begin
+            yield r,w,p
+          rescue Errno::EIO
+          end
+        end
+
+        $?.exitstatus
+      end
+    end
 
     def initialize(command)
       @command = command
     end
 
     def run(options = {})
-      PTY.spawn @command do |stdin, stdout, pid|
+      SafePTY.spawn @command do |stdin, stdout, pid|
         stdin.each do |output|
           begin
             if block_given?
