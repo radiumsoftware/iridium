a5a99524abbc5c50bd2a584aab8153c782d45e5a
twinturbo <me@broadcastingadam.com>
2012-08-31 21:21:27 +0200

    Test apps work in standard environments

--- lib/iridium/cli.rb
+++ lib/iridium/cli.rb
@@ -33,8 +33,9 @@ module Iridium
     desc "compile PATH", "compile assets for deployment to an optional PATH"
     method_option :environment, :aliases => '-e', :default => 'production'
     def compile(path = nil)
-      Iridium.load!
       ENV['IRIDIUM_ENV'] = options[:environment]
+
+      Iridium.load!
 
       if path
         raise "#{path} does not exist!" unless File.directory? path
--- test/integration/new_app_test.rb
+++ test/integration/new_app_test.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
 
+# NOTE: Make sure each generated app is unique so it can be required
+# and the inherited callback will hit. This makes things work as expected
 class NewAppTest < MiniTest::Unit::TestCase
   def setup
     super
@@ -18,7 +20,7 @@ class NewAppTest < MiniTest::Unit::TestCase
       assert File.directory?('todos'), "App not generated!"
     end
 
-    Iridium.application = nil # so the new app is loaded
+    Iridium.application = nil
 
     Dir.chdir sandbox_path.join('todos') do
       # have to explicitly declare test files because default ARGV
@@ -30,12 +32,65 @@ class NewAppTest < MiniTest::Unit::TestCase
         result = Iridium::Commands::Test.start test_files
       end
 
-      assert_equal 0, result, "RELEASE BLOCKER: Tests failed for some reason!\n#{stdout}"
+      refute_equal 1, result, "RELEASE BLOCKER: Tests failed: #{stdout}"
+      refute_equal 2, result, "RELEASE BLOCKER: Test suite failed to start: #{stderr}"
+      assert_equal 0, result
 
       assert_empty stderr
       assert_includes stdout, "2 Test(s)"
       assert_includes stdout, "0 Error(s)"
       assert_includes stdout, "0 Failure(s)"
     end
+  end
+
+  def test_app_works_works_in_development
+    Dir.chdir sandbox_path do
+      capture_io { Iridium::CLI.start %w(app dev_test) }
+
+      assert File.directory?('dev_test'), "App not generated!"
+    end
+
+    Iridium.application = nil
+
+    Dir.chdir sandbox_path.join('dev_test') do
+      runner = Iridium::CLI.new [], :environment => 'development'
+      runner.invoke :compile
+    end
+  ensure
+    ENV['IRIDIUM_ENV'] = 'test'
+  end
+
+  def test_app_works_works_in_test
+    Dir.chdir sandbox_path do
+      capture_io { Iridium::CLI.start %w(app test_test) }
+
+      assert File.directory?('test_test'), "App not generated!"
+    end
+
+    Iridium.application = nil
+
+    Dir.chdir sandbox_path.join('test_test') do
+      runner = Iridium::CLI.new [], :environment => 'test'
+      runner.invoke :compile
+    end
+  ensure
+    ENV['IRIDIUM_ENV'] = 'test'
+  end
+
+  def test_app_works_works_in_production
+    Dir.chdir sandbox_path do
+      capture_io { Iridium::CLI.start %w(app production_test) }
+
+      assert File.directory?('production_test'), "App not generated!"
+    end
+
+    Iridium.application = nil
+
+    Dir.chdir sandbox_path.join('production_test') do
+      runner = Iridium::CLI.new [], :environment => 'production'
+      runner.invoke :compile
+    end
+  ensure
+    ENV['IRIDIUM_ENV'] = 'test'
   end
 end
