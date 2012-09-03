ae358aebacf3b10807c4bb483dd436aa8a766793
twinturbo <me@broadcastingadam.com>
2012-09-03 10:06:25 +0200

    Add failing tests

--- test/config_test.rb
+++ test/config_test.rb
@@ -28,4 +28,32 @@ class ConfigTest < MiniTest::Unit::TestCase
     assert_equal 1, config.proxies.size
     assert_equal 'bar.com', config.proxies['/api']
   end
+
+  def test_load_add_dependences
+    config = Iridium::Config.new
+
+    config.dependencies.load :foo
+
+    assert_includes config.dependencies, :foo
+  end
+
+  def test_unload_removes_dependences
+    config = Iridium::Config.new
+
+    config.dependencies << :foo
+    config.dependencies.unload :foo
+
+    assert_empty config.dependencies
+  end
+
+  def test_swap_dependencies
+    config = Iridium::Config.new
+
+    config.dependencies.load :handlebars
+
+    config.dependencies.swap :handlebars, :handlebars_runtime
+
+    assert_includes config.dependencies, :handlebars_runtime
+    refute_includes config.dependencies, :handlebars
+  end
 end
