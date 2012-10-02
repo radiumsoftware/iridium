71c7727156bb5589558aa4d868b8d2a9b4b4d566
twinturbo <me@broadcastingadam.com>
2012-10-02 19:52:30 +0200

    Beginning implmenting the ground work

---
+++ test/component_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+require 'iridium/component'
+
+class ComponentTest < MiniTest::Unit::TestCase
+  class MockMiddleware ; end
+
+  def test_components_can_configure_the_middleware_stack
+    Class.new Iridium::Component do
+      middleware.use MockMiddleware
+    end
+  end
+
+  def test_components_share_the_same_middleware_stack
+    component1 = Class.new Iridium::Component
+    component2 = Class.new Iridium::Component
+
+    assert_equal component1.middleware, component2.middleware
+  end
+end
