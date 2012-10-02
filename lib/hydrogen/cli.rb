7089f61db8a9650058da2239487f2f423f68d890
twinturbo <me@broadcastingadam.com>
2012-10-02 19:19:35 +0200

    Specify extends and includes in components

--- lib/hydrogen/cli.rb
+++ lib/hydrogen/cli.rb
@@ -2,10 +2,12 @@ module Hydrogen
   class CLI < Thor
     class << self
       def inherited(base)
-        Component.commands.each do |hash|
-          command_class, command_name = hash[:class], hash[:name]
-          base.desc "#{command_name} command ARGS", command_class.description_banner
-          base.subcommand command_name.to_s, command_class
+        Component.loaded.each do |component|
+          component.commands.each do |hash|
+            command_class, command_name = hash[:class], hash[:name]
+            base.desc "#{command_name} command ARGS", command_class.description_banner
+            base.subcommand command_name.to_s, command_class
+          end
         end
       end
     end
