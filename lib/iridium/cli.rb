44795941d735f491a54d1baaeaa25b9eb483ad85
twinturbo <me@broadcastingadam.com>
2012-10-12 16:35:50 +0200

    Reorganizing tests

--- lib/iridium/cli.rb
+++ lib/iridium/cli.rb
@@ -1,21 +1,4 @@
-require 'iridium/commands/application'
-require 'thor/group'
-
 module Iridium
-  class CLI < Thor
-    class << self
-      def subcommand_with_default(subcommand, subcommand_class)
-        self.subcommands << subcommand.to_s
-        subcommand_class.subcommand_help subcommand
-
-        define_method(subcommand) do |*args|
-          args, opts = Thor::Arguments.split(args)
-          invoke subcommand_class, [subcommand_class.default_task, args].flatten, opts
-        end
-      end
-    end
-
-    desc "app PATH", "generate a new application in PATH"
-    subcommand_with_default "app", Commands::Application
+  class CLI < Hydrogen::CLI
   end
 end
