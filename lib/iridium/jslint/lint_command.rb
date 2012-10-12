87d205ce0dc9155f2bd0a2dc10176ef46f3ffa6a
twinturbo <me@broadcastingadam.com>
2012-10-12 16:55:03 +0200

    Add thor requirements

--- lib/iridium/jslint/lint_command.rb
+++ lib/iridium/jslint/lint_command.rb
@@ -1,14 +1,13 @@
 module Iridium
   module JSLint
-    class LintCommand
-      class << self
-        def start(args = ARGV)
-          if args.size == 0
-            args = Dir['app/javascripts/**/*.js']
-          end
-
-          JSLintRunner.execute args
+    class LintCommand < Hydrogen::Command
+      desc "lint FILES", "lint the specified files"
+      def lint(*files)
+        if files.size == 0
+          files = Dir['app/javascripts/**/*.js']
         end
+
+        JSLintRunner.execute files
       end
     end
   end
