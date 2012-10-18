f4b949aff201879bbf6b77a8cac4a9f281840f5a
twinturbo <me@broadcastingadam.com>
2012-10-18 15:46:28 +0200

    More engine fixes

--- test/engine_test.rb
+++ test/engine_test.rb
@@ -1,30 +1,28 @@
 require 'test_helper'
 
 class EngineTest < MiniTest::Unit::TestCase
-  def test_app_paths
-    component = Iridium::Engine.new
+  def app
+    Iridium.application
+  end
 
-    assert_equal "app", component.paths[:app].first.path
-    assert_equal "app/config/initializers", component.paths[:initializers].first.path
-    assert_equal "app/locales", component.paths[:locales].first.path
-    assert_equal "app/javascripts", component.paths[:javascripts].first.path
-    assert_equal "app/templates", component.paths[:templates].first.path
-    assert_equal "app/stylesheets", component.paths[:stylesheets].first.path
-    assert_equal "app/assets", component.paths[:assets].first.path
-    assert_equal "app/sprites", component.paths[:sprites].first.path
+  def test_app_paths
+    assert_equal "app", app.paths[:app].first.path
+    assert_equal "app/config/initializers", app.paths[:initializers].first.path
+    assert_equal "app/locales", app.paths[:locales].first.path
+    assert_equal "app/javascripts", app.paths[:javascripts].first.path
+    assert_equal "app/templates", app.paths[:templates].first.path
+    assert_equal "app/stylesheets", app.paths[:stylesheets].first.path
+    assert_equal "app/assets", app.paths[:assets].first.path
+    assert_equal "app/sprites", app.paths[:sprites].first.path
   end
 
   def test_vendor_path
-    component = Iridium::Engine.new
-
-    assert_equal "vendor", component.paths[:vendor].first.path
+    assert_equal "vendor", app.paths[:vendor].first.path
   end
 
   def test_build_paths
-    component = Iridium::Engine.new
-
-    assert_equal "site", component.paths[:site].first.path
-    assert_equal "tmp", component.paths[:tmp].first.path
-    assert_equal "tmp/build", component.paths[:build].first.path
+    assert_equal "site", app.paths[:site].first.path
+    assert_equal "tmp", app.paths[:tmp].first.path
+    assert_equal "tmp/build", app.paths[:build].first.path
   end
 end
