56f03d2c1faf341dcfd40b55fc041772a7f8000a
twinturbo <me@broadcastingadam.com>
2012-10-19 12:24:00 +0200

    Implemented tests passing

--- lib/rake-pipeline/iridium_helper.rb
+++ lib/rake-pipeline/iridium_helper.rb
@@ -11,6 +11,14 @@ end
 
 module Rake
   class Pipeline
+    class Matcher
+      include IridiumHelper
+    end
+  end
+end
+
+module Rake
+  class Pipeline
     module DSL
       class PipelineDSL
         include IridiumHelper
@@ -22,6 +30,33 @@ module Rake
           matcher
         end
         alias :skip :drop
+
+        def sort(&block)
+          sorter = pipeline.copy(SortedPipeline)
+          sorter.sorter = block
+          pipeline.add_filter sorter
+          sorter
+        end
+
+        def engines_first
+          sort do |f1, f2|
+            in_engine = lambda do |file|
+              engines = Iridium::Engine.subclasses
+              engines.delete pipeline.app.class
+              engines.any? do |engine|
+                file.fullpath.include? engine.instance.root.to_s
+              end
+            end
+
+            if in_engine.call(f1) && !in_engine.call(f2)
+              -1
+            elsif in_engine.call(f2) && !in_engine.call(f1)
+              1
+            else
+              0
+            end
+          end
+        end
       end
 
       class ProjectDSL
