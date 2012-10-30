84bdd2880dd72a20f517b2089d60aad57e5d2e45
twinturbo <me@broadcastingadam.com>
2012-10-30 17:19:43 +0100

    vm should be runtime

--- lib/iridium/pipeline.rb
+++ lib/iridium/pipeline.rb
@@ -61,7 +61,7 @@ module Iridium
     config.scripts = Pipeline::DependencyArray.new
 
     config.dependencies.load :minispade, :handlebars, :i18n, :jquery
-    config.dependencies.skip "handlebars-vm"
+    config.dependencies.skip "handlebars-runtime"
 
     class << self
       def before_compile(*args, &block)
