1eb583182ecaa7373dc9aa573e784b8243854f33
twinturbo <me@broadcastingadam.com>
2012-11-06 09:39:41 +0100

    Don't use symlinked Assetfile (Close #30)

--- lib/iridium/generators/assetfile_generator.rb
+++ lib/iridium/generators/assetfile_generator.rb
@@ -1,6 +1,8 @@
 module Iridium
   module Generators
     class AssetfileGenerator < Iridium::Generator
+      self.source_root File.expand_path "../../pipeline", __FILE__
+
       desc "Generate an Assetfile"
       def assetfile
         copy_file "Assetfile"
