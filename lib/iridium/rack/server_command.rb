04ec3c53e8844c1e250bd149e648ef36bc5383bc
twinturbo <me@broadcastingadam.com>
2012-12-05 11:03:07 +0100

    iridium server -e

--- lib/iridium/rack/server_command.rb
+++ lib/iridium/rack/server_command.rb
@@ -4,8 +4,10 @@ module Iridium
       description "Start a preview server"
 
       desc "server", "start a development server"
+      method_option :environment, :aliases => '-e', :default => 'development'
       def server
-        ENV['IRIDIUM_ENV'] = 'development'
+        ENV['IRIDIUM_ENV'] = options[:environment]
+
         Iridium.load!
         Iridium.application.clean!
         Iridium::Rack::DevServer.new.start
