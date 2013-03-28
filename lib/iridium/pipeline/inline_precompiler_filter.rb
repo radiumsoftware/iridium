0d664a5016b82f68da4e1ae4c3964c0989353b9d
twinturbo <me@broadcastingadam.com>
2013-03-28 19:58:11 +0100

    Fix regex issue

--- lib/iridium/pipeline/inline_precompiler_filter.rb
+++ lib/iridium/pipeline/inline_precompiler_filter.rb
@@ -3,7 +3,7 @@ module Iridium
     class InlinePrecompilerFilter < ::Rake::Pipeline::Filter
       def generate_output(inputs, output)
         inputs.each do |input|
-          precompiled_content = input.read.gsub(/Handlebars\.compile\(.+\)/) do |match|
+          precompiled_content = input.read.gsub(/Handlebars\.compile\((.+)\)/m) do |match|
             InlineHandlebarsPrecompiler.call $1
           end
 
